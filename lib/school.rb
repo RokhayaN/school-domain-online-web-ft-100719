d0e02d8d7b07da2ce29ffd52c824cbb672b76258
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-10-18 04:12:49 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,8 +1,9 @@
 class School# code here!
 end
 def initialize (school)
-school = School.new("Bayside High School")
+school = School.new (school)
 end
 def school.roster
-  {}
+  hash={}
 end
+def school.add_student ()
