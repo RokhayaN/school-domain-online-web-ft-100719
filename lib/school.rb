3abfb6837b6e28ea31243b8025b4d1ca73468e63
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-10-18 04:15:21 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,3 +9,8 @@ hash["new_key"] = []
 hash["new_key"] << "new_value_for_value_array"
 end
 def school.add_student ()
+  school.add_student("AC Slater", 9)
+school.add_student("Kelly Kapowski", 10)
+school.add_student("Screech", 11)
+school.roster
+end
