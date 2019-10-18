347b9e456f9e5f39a008cd867fe360f93ea89b76
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-10-18 04:14:38 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -4,6 +4,8 @@ def initialize (school)
 school = School.new (school)
 end
 def school.roster
-  hash={}
+hash={}
+hash["new_key"] = []
+hash["new_key"] << "new_value_for_value_array"
 end
 def school.add_student ()
